f3d4304375ac0e760e7eed0732489f2f7d1ae234
jack <wj903829182@sina.cn>
2018-04-21 22:23:02 +0800

    Initial commit study webflux

---
+++ webflux1/src/test/java/com/jack/webflux1/Webflux1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jack.webflux1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Webflux1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
