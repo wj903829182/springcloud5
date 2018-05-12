2d6f2c6151f73af5e351f52c14d5604ed896a4c3
jack <wj903829182@sina.cn>
2018-05-12 11:51:16 +0800

    webflux 和thymeleaf

---
+++ webflux_thymeleaf/src/main/java/com/jack/webflux_thymeleaf/WebfluxThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.jack.webflux_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxThymeleafApplication.class, args);
+	}
+}
---
+++ webflux_thymeleaf/src/test/java/com/jack/webflux_thymeleaf/WebfluxThymeleafApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jack.webflux_thymeleaf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebfluxThymeleafApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
