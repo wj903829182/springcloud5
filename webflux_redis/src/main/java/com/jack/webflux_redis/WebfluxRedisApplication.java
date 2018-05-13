f88f114d8daaf15cb7d0052520a35b202018e0bb
jack <wj903829182@sina.cn>
2018-05-13 09:39:52 +0800

    webflux 和redis

---
+++ webflux_redis/src/main/java/com/jack/webflux_redis/WebfluxRedisApplication.java
@@ -0,0 +1,12 @@
+package com.jack.webflux_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxRedisApplication.class, args);
+	}
+}
