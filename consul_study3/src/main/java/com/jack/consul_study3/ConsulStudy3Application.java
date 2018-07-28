0ad2c231fd183d567b02a35cafdd5794ed1f04c4
jack <wj903829182@sina.cn>
2018-07-28 10:23:27 +0800

    consul学习3

---
+++ consul_study3/src/main/java/com/jack/consul_study3/ConsulStudy3Application.java
@@ -0,0 +1,12 @@
+package com.jack.consul_study3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulStudy3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulStudy3Application.class, args);
+	}
+}
---
+++ consul_study3/src/test/java/com/jack/consul_study3/ConsulStudy3ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jack.consul_study3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsulStudy3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
