5025d1bcce64dce5e1e36da71acceb280f0de84d
jack <wj903829182@sina.cn>
2018-08-04 08:32:30 +0800

    mybatisplus学习1

---
+++ mybatisplus_study1/src/main/java/com/jack/mybatisplus_study1/MybatisplusStudy1Application.java
@@ -0,0 +1,12 @@
+package com.jack.mybatisplus_study1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisplusStudy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisplusStudy1Application.class, args);
+	}
+}
---
+++ mybatisplus_study1/src/test/java/com/jack/mybatisplus_study1/MybatisplusStudy1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jack.mybatisplus_study1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybatisplusStudy1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
