0b988d00227f739ddceb0cba68e2d4217186758e
jack <wj903829182@sina.cn>
2018-08-19 10:38:12 +0800

    springboot接口版本控制

---
+++ springboot_version/src/main/java/com/jack/springboot_version/SpringbootVersionApplication.java
@@ -0,0 +1,12 @@
+package com.jack.springboot_version;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootVersionApplication.class, args);
+	}
+}
