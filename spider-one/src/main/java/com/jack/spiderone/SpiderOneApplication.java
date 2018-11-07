5f1092f3ef3efadd45c14b63459c363323addb56
jack <wj903829182@sina.cn>
2018-11-07 22:13:21 +0800

    java网络爬虫学习

---
+++ spider-one/src/main/java/com/jack/spiderone/SpiderOneApplication.java
@@ -0,0 +1,12 @@
+package com.jack.spiderone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiderOneApplication.class, args);
+	}
+}
