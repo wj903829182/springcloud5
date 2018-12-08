f289d0cae46d43a33be5915fdd722bc13fda8551
jack <wj903829182@sina.cn>
2018-12-08 13:41:37 +0800

    shiro自定义realm

---
+++ shirostudyone/src/main/java/com/jack/realm/CustomRealm.java
@@ -0,0 +1,108 @@
+package com.jack.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * create by jack 2018/12/8
+ *
+ * @author jack
+ * @date: 2018/12/8 09:31
+ * @Description:
+ * 自定义realm
+ */
+public class CustomRealm extends AuthorizingRealm {
+    /**
+     * 模拟数据库中的用户名和密码
+     */
+    private Map<String, String> userMap = new HashMap<String, String>(16);
+
+    /**
+     * 使用代码块初始化数据
+     */
+    {
+        userMap.put("jack", "123456");
+        super.setName("customRealm");
+    }
+    /**
+     * 用于授权
+     * @param principalCollection
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        String username = (String) principalCollection.getPrimaryPrincipal();
+        //从数据库或者缓存中获取角色数据
+        Set<String> roles = getRolesByUsername(username);
+        //从数据库或者缓存中获取权限数据
+        Set<String> permissions = getPerminssionsByUsername(username);
+        //创建AuthorizationInfo，并设置角色和权限信息
+        SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+        authorizationInfo.setStringPermissions(permissions);
+        authorizationInfo.setRoles(roles);
+        return authorizationInfo;
+    }
+
+    /**
+     * 模拟通过数据库获取权限数据
+     * @param username
+     * @return
+     */
+    private Set<String> getPerminssionsByUsername(String username) {
+        Set<String> permissions = new HashSet<String>();
+        permissions.add("user:delete");
+        permissions.add("user:add");
+        return permissions;
+    }
+
+    /**
+     * 模拟通过数据库获取用户角色信息
+     * @param username
+     * @return
+     */
+    private Set<String> getRolesByUsername(String username) {
+        Set<String> roles = new HashSet<String>();
+        roles.add("admin");
+        roles.add("user");
+        return roles;
+    }
+
+    /**
+     * 用于认证
+     * @param authenticationToken 传过来的认证信息
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        //从主体传过来的认证信息获取登入的用户名
+        String username = (String) authenticationToken.getPrincipal();
+        //通过用户名到数据库获取凭证
+        String password = getPasswordByUsername(username);
+        if (password == null) {
+            return null;
+        }
+        SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(username,password,"customRealm");
+        return authenticationInfo;
+    }
+
+    /**
+     * 通过用户名查询密码，模拟数据库查询
+     * @param username
+     * @return
+     */
+    private String getPasswordByUsername(String username) {
+        return userMap.get(username);
+    }
+}
---
+++ shirostudyone/src/main/java/com/jack/test/CutomRealmTest.java
@@ -0,0 +1,86 @@
+package com.jack.test;
+
+import com.jack.realm.CustomRealm;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.text.IniRealm;
+import org.apache.shiro.subject.Subject;
+
+/**
+ * create by jack 2018/12/8
+ *
+ * @author jack
+ * @date: 2018/12/8 09:49
+ * @Description:
+ */
+public class CutomRealmTest {
+
+
+    /**
+     * 自定义用户认证
+     */
+    public static void cutomRealmTest1(){
+
+        //使用自定义Realm
+        CustomRealm customRealm = new CustomRealm();
+
+        //1,构建SecurityManager环境
+        DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+        //设置Realm
+        defaultSecurityManager.setRealm(customRealm);
+
+        //2,主体提交认证请求
+        SecurityUtils.setSecurityManager(defaultSecurityManager);
+        //获取主体
+        Subject subject = SecurityUtils.getSubject();
+        //用户名和密码的token
+        UsernamePasswordToken token = new UsernamePasswordToken("jack", "123456");
+        //进行登入
+        subject.login(token);
+        System.out.println("是否权限认证：isAuthenticated=" + subject.isAuthenticated());
+        //检查是否有角色
+        //subject.checkRoles("admin");
+        //System.out.println("有admin角色");
+        //检查是否有权限
+        //subject.checkPermissions("user:delete");
+        //System.out.println("有user:delete权限");
+
+    }
+
+    /**
+     * 自定义用户授权，权限
+     */
+    public static void cutomRealmTest2(){
+
+        //使用自定义Realm
+        CustomRealm customRealm = new CustomRealm();
+
+        //1,构建SecurityManager环境
+        DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+        //设置Realm
+        defaultSecurityManager.setRealm(customRealm);
+
+        //2,主体提交认证请求
+        SecurityUtils.setSecurityManager(defaultSecurityManager);
+        //获取主体
+        Subject subject = SecurityUtils.getSubject();
+        //用户名和密码的token
+        UsernamePasswordToken token = new UsernamePasswordToken("jack", "123456");
+        //进行登入
+        subject.login(token);
+        System.out.println("是否权限认证：isAuthenticated=" + subject.isAuthenticated());
+        //检查是否有角色
+        subject.checkRoles("admin");
+        //System.out.println("有admin角色");
+        //检查是否有权限
+        subject.checkPermissions("user:delete","user:add");
+        //System.out.println("有user:delete权限");
+
+    }
+
+    public static void main(String[] args) {
+        //cutomRealmTest1();
+        cutomRealmTest2();
+    }
+}
